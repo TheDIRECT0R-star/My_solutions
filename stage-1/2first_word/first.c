a12e94018a5924f2a82156487b18fd805243642e
uranos <asiaresama@gmail.com>
2025-06-13 20:37:48 +0100

    second first word practice makes perfect suiii

---
+++ stage-1/2first_word/first.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+
+	int i = 0;
+
+	while (argv[1][i] == ' ' || argv[1][i] == '\t')
+	{
+		i++;
+	}
+
+	while (argv[1][i])
+	{
+		write(1, &argv[1][i], 1);
+		
+		if (argv[1][i] == ' ' || argv[1][i] == '\t')
+		{
+			return 0;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+	return 0;
+}
